722c698ffddf246ae4532068dbedd3a661677817
Gerald Craig <geraldcraig@me.com>
2024-04-02 13:17:48 +0100

    frontendupdated

---
+++ frontend-app/src/components/Layout.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function AboutTheApp() {
+  return (
+    <div>
+      <h1>About the App</h1>
+      <p>This app is a full stack application that uses React for the frontend and Node.js for the backend.</p>
+    </div>
+  );
+}
